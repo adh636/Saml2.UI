efb3f0aecbcc49c6a15d4ac9c57e87f5c1d30a60
Adam Hilliard <adh636@gmail.com>
2018-01-07 23:54:12 -0500

    Get token call to auth service

--- src/app/saml-login/saml-login.component.ts
+++ src/app/saml-login/saml-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-saml-login',
@@ -6,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./saml-login.component.css']
 })
 export class SamlLoginComponent implements OnInit {
+  private token: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  getToken(loginResponse: any) {
+    const body: any = {
+      grant_type: 'saml20',
+      assertion: btoa(loginResponse)
+    };
+    const httpOptions: any = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+    this.http.post('http://localhost:25009/CineNet/Authorization/Token', body, httpOptions)
+      .subscribe((token: any) => {
+        this.token = token;
+        // this.redirectEndpoint();
+      });
   }
 }
